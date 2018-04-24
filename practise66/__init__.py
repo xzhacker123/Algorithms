94d350e5dac27dd3cacebca71d3deee5be39a25e
Alex <xzhacker@163.com>
2018-04-24 16:49:11 +0800

    first

---
+++ practise66/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/24 16:47
+# @Author  : Alex.xu
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ practise66/demo.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/24 16:47
+# @Author  : Alex.xu
+# @File    : demo.py
+# @Software: PyCharm
